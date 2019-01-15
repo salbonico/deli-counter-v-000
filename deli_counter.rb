6d66df4173485de64e4a9210d933b18669abdec7
Steve Albonico <salbonico@gmail.com>
2019-01-15 21:35:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,3 +6,9 @@ if queue.size == 0
   puts "The line is currently empty."
 end
 end
+
+def take_a_number(queue, name)
+  queue.push(name)
+  position = queue.size
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
